e729d185e2d115e4c3c09dc92eaa18bb1afcc17c
Melissa Guzman <melissa.guzman@outlook.com>
2020-07-26 15:50:30 -0700

    updates index to connect to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require("express");
-const bodyParser = require("body-parser")
-const dogs = require ("./data");
+const bodyParser = require("body-parser");
+const Dog = require("./models/dogs");
 
 const app = express();
 
@@ -20,17 +20,39 @@ app.use(express.static(__dirname + '/public' ));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-let displayDogs = dogs.getAll();
+//let displayDogs = dogs.getAll();
 
-app.get('/', (req, res) => {
-  res.type('text/html');
-  res.render('home', {dogs: displayDogs});
+app.get('/', (req, res, next) => {
+  return Dog.find({}).lean()
+      .then((dogs) => {
+          res.render('home', { dogs });
+      })
+      .catch(err => next(err));
 });
 
 app.get('/detail', (req, res) => {
-  const dogname = req.query.name
-  res.render('detail', {dogs: dogname, info: dogs.getDetail(dogname)});
+  const dogname = req.query.name;
+  Dog.findOne({name: dogname}).lean()
+  .then((dogs) => {
+      res.render('detail', {name: dogname, info: dogs});
+  });
 });
+app.get('/delete', (req, res) => {
+  const dogname = req.query.name;
+  Dog.findAndDelete({name: dogname}, (err, dog) => {
+    
+      if (err) {
+          console.log(err);
+      } else if (!dog) {
+          console.log(dogname + " not found");
+          res.send(`${dogname} not found`);
+      } else if (dog) {
+          console.log(dogname + " delete successful");
+          res.send(`${dogname} delete successful`);
+      }
+  });
+});
+
 
 app.get('/about', (req, res) => {
   res.type('text/plain');
